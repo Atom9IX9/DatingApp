fbbbfb1a577f63cabeafad290158f85cb7fa43ca
Atom9IX9 <vorobjovaroslav33@gmail.com>
2026-01-10 12:42:17 +0200

    Changes for startpage

--- app/src/app/(provided)/(guest)/page.tsx
+++ app/src/app/(provided)/(guest)/page.tsx
@@ -1,19 +1,22 @@
 import { Box } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
+import style from "./guestPages.module.scss";
 
 const HomePage: React.FC = async () => {
   return (
-    <Box>
-      start unauth page 
-      <div>
-        <Link href="sign-in">Sign in</Link>
-      </div>
-      <div>
-        <Link href="sign-up">Sign up</Link>
-      </div>
+    <Box sx={{ display: "flex", height: "100vh" }}>
+      <Box component="section" sx={{ width: "55%", position: "relative" }}>
+        <Image
+          src="/imgs/hero.png"
+          alt="hero"
+          sizes="40vw"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </Box>
     </Box>
   );
 };
 
 export default HomePage;
-
