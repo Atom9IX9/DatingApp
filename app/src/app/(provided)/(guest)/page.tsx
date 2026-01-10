8db155468d3c5f4622f39f4fec2a85903f20ae9f
Atom9IX9 <vorobjovaroslav33@gmail.com>
2026-01-10 18:28:02 +0200

    New hero-img

--- app/src/app/(provided)/(guest)/page.tsx
+++ app/src/app/(provided)/(guest)/page.tsx
@@ -6,13 +6,18 @@ import style from "./guestPages.module.scss";
 const HomePage: React.FC = async () => {
   return (
     <Box sx={{ display: "flex", height: "100vh" }}>
-      <Box component="section" sx={{ width: "55%", position: "relative" }}>
+      <Box component="section" sx={{ width: "64%", position: "relative" }}>
         <Image
-          src="/imgs/hero.png"
+          src="/imgs/large-hero.png"
           alt="hero"
-          sizes="40vw"
+
+          sizes="64vw"
           fill
-          style={{ objectFit: "cover" }}
+          style={{ 
+            objectFit: "cover",
+            objectPosition: "right top",
+            opacity: 0.8,
+          }}
         />
       </Box>
     </Box>
